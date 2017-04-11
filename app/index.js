87d243682a90a65ed1dfea1264d4f900886a4997
Dylan Swoope <dylanswoope@Dylans-MacBook-Pro.local>
2017-04-10 20:14:19 -0500

    added react-modal; component rendering modal when list item clicked

--- app/index.js
+++ app/index.js
@@ -1,37 +1,55 @@
-import Avatar from './avatar.js';
-import SiteData from './models/site_data.js';
+import SiteData from './models/site_list_data.js';
 import SiteCard from './site_card.js';
-
-var AvatarData = {
-	name: "Dylan SHWOOPS",
-	username: "dylanshwerps",
-	image: "http://www.absoluteanime.com/neon_genesis_evangelion/misato.jpg"
-}
+import SiteDetail from './site_detail.js';
+import Modal from 'react-modal';
+import ModalStyles from './modal_styles.js'
 
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 const SiteList = React.createClass({
-  render: function () {
-    const list = this.props.site_list.map(site => {
+  getInitialState: function() {
+    return { 
+      modalIsOpen: false,
+      modalData: null,
+      sites: this.props.site_list
+    };
+  },
+ 
+  openModal: function(i) {
+    this.setState({modalIsOpen: true, modalData: this.state.sites[i]});
+  },
+ 
+  closeModal: function() {
+    this.setState({modalIsOpen: false});
+  },
+
+  renderSiteList: function() {
+    console.log("modal data: ", this.state)
+    const list = this.state.sites.map((site, index) => {
       return (
-        <li><SiteCard data={site} /></li>
+        <li key={index} onClick={()=> this.openModal(index)}><SiteCard data={site}/></li>
       )
     })
     return (
       <ul>
         {list}
       </ul>
-     
-    )
+    );
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.renderSiteList()}
+        <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal} contentLabel="Modal" style={ModalStyles}>
+          <SiteDetail data={this.state.modalData} />
+        </Modal>
+      </div>
+    );
   }
 })
 
-
-ReactDOM.render(
-  <Avatar user={AvatarData} />,
-  document.getElementById('avatar_test')
-);
 
 ReactDOM.render(
   <SiteList site_list={SiteData} />,
---
+++ app/site_detail.js
@@ -0,0 +1,68 @@
+//Site Detail card pops up as modal window when list item clicked on
+//Contains:
+//Title Bar [Site Name, Address,  site ID]
+//Site Info [lat long, watershed]
+//Data [current GH, Q, Precip, change in GH and Q over the past hour]
+//Expand Graph [link to expand GH graph] --> make request for this when initial modal opens
+var React = require('react');
+
+var TitleBar = React.createClass({
+  render: function() {
+    return (
+    <span>{this.props.site_name}</span>
+    )
+  }
+})
+
+var CurrentConditions = React.createClass({
+  render: function() {
+    return (
+      <span>This is where current condition info will go</span>
+    )
+  }
+})
+
+var SiteInfo = React.createClass({
+  render: function() {
+    return (
+      <span>This is where location info will go</span>
+    )
+  }
+})
+
+var HydroGraph = React.createClass({
+  render: function() {
+    return (
+      <div className="graph_wrapper">
+        <img src={this.props.gh_graph_link} />
+      </div>
+    )
+  }
+})
+
+var ShowGraph = React.createClass({
+  render: function() {
+    return (
+      <a>Show Hydrograph</a>
+    )
+  }
+})
+
+
+
+
+var SiteDetail = React.createClass({
+  render: function () {
+    return (
+        <div className="site_detail site_modal">
+          <TitleBar site_name={this.props.data.site_name} />
+          <CurrentConditions gage_height={this.props.data.gage_height} />
+          <SiteInfo lat={this.props.data.latitude} long={this.props.data.longitude} />
+          <ShowGraph />
+          <HydroGraph graph_link={this.props.data.gh_graph_link} />
+        </div>
+    )
+  }
+})
+
+export default SiteDetail;
